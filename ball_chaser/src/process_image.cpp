337f2a19e56e5b90b22011ee073032ae57ad99ef
Sameh Hassan <samehsmh@gmail.com>
2019-09-14 16:43:03 -0700

    ball_chaser: process_image: Add ROS node functionality
    Add required ROS node functionality as defined in course:
    * process_image_callback:
      - Iterate over all pixels and check for white pixels.
      - If found, check surrounding pixels.
      - If a ball is found, detect if found in right, middle or left of the
        screen.
      - Select proper linear and angular velocities depending on location.
    * drive_robot:
      - Is called when a ball is found and proper velocities are set.
      - Send command to /ball_chaser/command_robot with velocities.

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -8,19 +8,68 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
+    //ROS_INFO_STREAM("Driving Robot");
+
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    // Call the safe_move service and pass the requested joint angles
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service drive_robot");
+}
+
+bool check_pixel(const std::vector<uint8_t> &data, uint32_t pos, uint32_t value)
+{
+    return ( data[pos] == value &&
+             data[pos+1] == value &&
+             data[pos+2] == value ) ? true : false;
 }
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
+    const uint8_t white_pixel = 255;
+    bool white_ball_exists = false;
+    float linear_x = 0.0;
+    float angular_z = 0.0;
+    uint32_t i = 0;
+    uint32_t j = 0;
 
-    int white_pixel = 255;
+    // Loop through each pixel in the image and check if there's a bright white one
+    for( i = 0; i < img.height; i++ ) {
+        for( j = 0; j < img.width; j++ ) {
+            if( check_pixel( img.data, i*img.step + j*3, white_pixel ) ) {
+                // Check surrounding pixels if they are the same color
+                if( check_pixel( img.data, i*img.step + (j+1)*3, white_pixel ) &&
+                    check_pixel( img.data, (i+1)*img.step + j*3, white_pixel ) ){
+                    // Check upper and left pixels (if in existing range)
+                    if( i > 0 && check_pixel( img.data, (i-1)*img.step + j*3, white_pixel ) ) {
+                        if( j > 0 && check_pixel( img.data, i*img.step + (j-1)*3, white_pixel ) ) {
+                            white_ball_exists = true;
+                            break;
+			}
+                    }
+                }
+            }
+        }
+        if( white_ball_exists )
+            break;
+    }
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
+    // Identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+    if( white_ball_exists ) {
+        linear_x = 0.1;
+        if( j < img.width / 3 )
+            angular_z = M_PI / (6 * 10);
+        else if( j > 2 * img.width / 3 )
+            angular_z = -M_PI / (6 * 10);
+    }
+
+    drive_robot(linear_x, angular_z);
 }
 
 int main(int argc, char** argv)
